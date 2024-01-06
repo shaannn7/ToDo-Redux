632e10207fc83d4fc5b1a7cf1da0fcab9f8e3b9e
shaannn7 <shaannn7>
2024-01-06 12:52:16 +0530

    first commit

---
+++ src/Store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit"
+import Reducer from './TODO/Reducer'
+
+const store = configureStore({
+    reducer: {
+        todo: Reducer,
+    }
+})
+export default store;
